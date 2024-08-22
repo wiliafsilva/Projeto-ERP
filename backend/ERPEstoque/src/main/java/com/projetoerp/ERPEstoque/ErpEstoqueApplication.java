f8480d02d40e7a86fe91e209b0c14a65c34bbac6
Wilia <wilia.francisco@souunit.com.br>
2024-08-22 20:07:36 -0300

    Inicio do Projeto

---
+++ backend/ERPEstoque/src/main/java/com/projetoerp/ERPEstoque/ErpEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.projetoerp.ERPEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpEstoqueApplication.class, args);
+	}
+
+}
