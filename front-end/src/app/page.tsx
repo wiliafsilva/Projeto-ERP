6b89d24b2c9de17c76ad94b500e8f2336eec37d9
Wilia Francisco da Silva <143839013+wiliafsilva@users.noreply.github.com>
2024-10-31 19:53:44 -0300

    Slide page - apresentação (#10)
    Slide page - apresentação

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,113 +1,177 @@
-import Image from "next/image";
+// pages/index.tsx
+"use client";
+import React from 'react';
+import styles from '../app/style/slide.module.css';
+import { useRouter } from 'next/navigation';
 
-export default function Home() {
+const HeroPage: React.FC = () => {
+  const router = useRouter();
+
+  const handleComecar = () => {
+    router.push('/login');
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      {/* Header */}
+      <header className={styles.header}>
+        <img src="https://i.pinimg.com/originals/5d/26/a1/5d26a173f443cbd190e34481438d474b.png" alt="Logo" className={styles.logo} />
+        <h1 className={styles.titulolog}>Gerenciador de Estoque</h1>
+        <nav>
+          <a href="#funcionalidades" className={styles.navLink}>FUNCIONALIDADES</a>
+          <a href="#faq" className={styles.navLink}>CONTATOS</a>
+          <a href="#precos" className={styles.navLink}>PREÇOS</a>
+          <a href="#precos" className={styles.navLink}>SAIBA MAIS</a>
+        </nav>
+      </header>
+
+      {/* Hero Section */}
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Sistema de Gerenciamento de Estoque</h1>
+          <p className={styles.heroText}>Equipe:</p>
+          <p className={styles.heroText1}>Franck Patrick Hora Vasconcelos</p>
+          <p className={styles.heroText1}>Murilo Pedral Mota</p>
+          <p className={styles.heroText1}>Rafael Menezes Gonçalves </p>
+          <p className={styles.heroText1}>Renê Mendonça Marinho</p>
+          <p className={styles.heroText1}>Richard de Souza Santos</p>
+          <p className={styles.heroText1}>Wilia Francisco da Silva</p>
         </div>
-      </div>
+        <div className={styles.heroImage}>
+          <img src="https://guardeaju.com.br/wp-content/uploads/2021/09/Onde-guardar-o-estoque-de-sua-empresa.jpg" alt="Imagem de estoque" className={styles.image} />
+        </div>
+      </section>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Índice</h1>
+          <p className={styles.heroText2}>- Problema</p>
+          <p className={styles.heroText2}>- Solução</p>
+          <p className={styles.heroText2}>- Ferramentas de Desenvolvimento</p>
+          <p className={styles.heroText2}>- Diferencial</p>
+          <p className={styles.heroText2}>- Solução Desenvolvida</p>
+          <p className={styles.heroText2}>- Evoluções Futuras</p>
+          <p className={styles.heroText2}>- Testes</p>
+          <p className={styles.heroText2}>- Equipe de Desenvolvimento</p>
+        </div>
+        <div className={styles.heroImage1}>
+          <img src="https://www.galpoessaopaulo.com.br/imagens/locacao-de-galpao-de-estoque-de-produtos-preco.jpg" alt="Imagem de estoque" className={styles.image} />
+        </div>
+      </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Problema</h1>
+          <p className={styles.heroText1}>O mercado do Sr. Renato enfrenta sérios desafios na gestão do estoque, resultando em perdas de produtos, desperdício por itens vencidos. Essa ineficiência impacta na satisfação dos clientes e a reputação do negócio.</p>
+        </div>
+        <div className={styles.heroImage}>
+          <img src="https://img.freepik.com/fotos-premium/area-de-armazenamento-desorganizada-com-caixas-e-detritos-espalhados-num-armazem-mal-iluminado-durante-o-dia_1247367-64014.jpg" alt="Imagem de estoque" className={styles.image} />
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Solução</h1>
+          <p className={styles.heroText2}>Implantar um sistema de gestão de estoque que permita cadastrar os produtos da loja e gerenciá-los de maneira prática e intuitiva, facilitando o acompanhamento das datas de validade e do nível de estoque disponível.</p>
+        </div>
+        <div className={styles.heroImage1}>
+          <img src="https://media.licdn.com/dms/image/D4D12AQESOexsgpXCxQ/article-cover_image-shrink_600_2000/0/1700481932955?e=2147483647&v=beta&t=N12hIB9xC03zIIdBnP_GDnU4FXP2TxcruAXAB5YdmrU" alt="Imagem de estoque" className={styles.image} />
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Ferramentas de Desenvolvimento</h1>
+          <p className={styles.heroText1}>As tecnologias utilizadas incluem um conjunto robusto de linguagens de programação, como TypeScript para o front-end e Java para o back-end, além de frameworks e bibliotecas como React Next, que facilita a criação de interfaces reativas, e Spring Boot, que estrutura APIs RESTful. O banco de dados PostgreSQL oferece confiabilidade e escalabilidade com suporte a transações ACID. As ferramentas de desenvolvimento englobam IntelliJ IDEA e Visual Studio, com gerenciamento realizado por Maven, Docker, Kubernetes e Git, enquanto Node.js e Java Virtual Machine suportam as plataformas front-end e back-end, respectivamente, com hospedagem na AWS. No hardware, o servidor Java Tomcat Servlet se destaca por seu alto desempenho e suporte a HTTP/2, assegurando eficiência e escalabilidade nas aplicações.</p>
+        </div>
+        <div className={styles.heroImage}>
+          <img src="https://tutoriaisexpress.com.br/wp-content/uploads/2024/09/tecnologia-capablog.jpg" alt="Imagem de estoque" className={styles.image} />
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Diferencial</h1>
+          <p className={styles.heroText2}>O sistema se destaca por sua funcionalidade e simplicidade de uso, apresentando uma interface intuitiva e cores harmoniosas que facilitam a navegação. Isso permite que os usuários aproveitem ao máximo todas as funcionalidades sem enfrentar dificuldades.</p>
+        </div>
+        <div className={styles.heroImage1}>
+          <img src="https://uva.br/conteudo/genios-ciencia-computacao-mentes-brilhantes-scaled.webp" alt="Imagem de estoque" className={styles.image} />
+        </div>
+      </section>
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Solução Desenvolvida</h1>
+        </div>
+      </section>
+
+      <section className={styles.heroSection}>
+        <div className={styles.heroImage1}>
+          <img src="/images/homePage.jpeg" alt="Imagem home" className={styles.image} />
+        </div>
+        <div className={styles.heroImage}>
+          <img src="images/loginPage.jpeg" alt="Imagem de login" className={styles.image} />
+        </div>
+      </section>
+
+      <section className={styles.heroSection}>
+        <div className={styles.heroImage1}>
+          <img src="/images/adicionarPage.jpeg" alt="Imagem home" className={styles.image}/>
+        </div>
+        <div className={styles.heroImage}>
+          <img src="images/pesquisaPage.jpeg" alt="Imagem de login" className={styles.image} />
+        </div>
+      </section>
+
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Evoluções Futuras</h1>
+          <p className={styles.heroText1}>- Notificações inteligentes (vencimentos, produtos com pouco estoque);<br /> - Integração com outros sistemas; <br /> - Aplicativo Mobile;<br /> - Compatibilidade com leitores de código de barras; <br /> - Uso de QR Code personalizados para gerenciar o armazenamento (WMS - Warehouse Management System)</p>
+        </div>
+        <div className={styles.heroImage}>
+          <img src="https://www.hgcode.com.br/arqConteudo/arqNoticia/foto.png" alt="Imagem de estoque" className={styles.image} />
+        </div>
+      </section>
+
+      <section className={styles.heroSection}>
+        <div className={styles.heroContent}> 
+          <h1 className={styles.heroTitle}>Testes</h1>
+          <p className={styles.heroText1}>- Testes Funcionais <br />- Testes de Desempenho <br />- Testes de Segurança <br /> - Testes de Usabilidade <br /> - Testes de Regressão <br /> - Testes de Aceitação</p>
+        </div>
+        <div className={styles.heroImage1}>
+          <img src="https://codecrush.com.br/static/images/articles/testes-de-software.png" alt="Imagem de estoque" className={styles.image} />
+        </div>
+      </section>
+
+
+
+      {/* Features Section */}
+      <section id="funcionalidades" className={styles.featuresSection}>
+        <h1 className={styles.heroTitle}>Equipe de Desenvolvimento</h1>
+        <div className={styles.featuresContainer}>
+          {[
+            { src: 'images/patrick.jpg', name: 'Franck Patrick Hora', role: 'DevOps' },
+            { src: 'images/murilo.jpg', name: 'Murilo Pedral Mota', role: 'Scrum Master' },
+            { src: 'images/rafael.jpg', name: 'Rafael Menezes Gonçalves', role: 'Desenvolvimento' },
+            { src: 'images/rene.jpg', name: 'Renê Mendonça Marinho', role: 'Product Owner ' },
+            { src: 'images/richard.jpg', name: 'Richard de Souza Santos', role: 'Desenvolvimento' },
+            { src: 'images/wilia.jpg', name: 'Wilia Francisco da Silva', role: 'Desenvolvimento' }
+          ].map((item, index) => (
+            <div key={index} className={styles.featureCard}>
+              <img src={item.src} alt={`Imagem ${index + 1}`} className={styles.featureImage} />
+              <p className={styles.descriptionText}>{item.name}</p>
+              <p className={styles.descriptionText}>Função: {item.role}</p>
+            </div>
+          ))}
+        </div>
+        <button className={styles.heroButton} onClick={handleComecar}>COMECE A USAR AGORA!</button>
+      </section>
+
+      {/* Footer */}
+      <footer className={styles.footer}>
+        <p>&copy; 2024 Projeto ERP. Todos os direitos reservados.</p>
+      </footer>
+    </div>
   );
-}
+};
+
+export default HeroPage;
