fde9bc444efc9f1a0f2b945c0d6f08d5f8f85373
Richard Souza <souzza.richard25@gmail.com>
2024-11-09 21:07:15 -0300

    Habilita requisições cross origin

--- backend/ERPEstoque/src/main/java/com/projetoerp/ERPEstoque/Controllers/StockController.java
+++ backend/ERPEstoque/src/main/java/com/projetoerp/ERPEstoque/Controllers/StockController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Tag(name = "Endpoint de Produtos do Estoque")
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 @RequestMapping("/estoque")
 public class StockController {
 
